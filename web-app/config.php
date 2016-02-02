93cca8037d63cd9070670b87b2da03a7565b07b6
Luke Klinker <lklinker1@gmail.com>
2016-02-02 15:25:58 -0600

    oops, correct the web host

--- web-app/config.php
+++ web-app/config.php
@@ -1,4 +1,4 @@
 <?php
 $_authorization = "AIzaSyB46bQF9oL9QdOb48LWvbrBfvToik_tyRw";  // from the google developer console (API Key)
-$_web_host = "https://push.klinker.rhcloud.com"; // without the "/" at the end
+$_web_host = "https://push-klinkerapps.rhcloud.com"; // without the "/" at the end
 ?>
