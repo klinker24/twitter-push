1cc9676a031b45696f6bb3ab57b8cc1bb81bd2e9
Luke Klinker <lklinker1@gmail.com>
2016-02-02 19:07:56 -0600

    log some password output to test

--- web-app/write_subscription.php
+++ web-app/write_subscription.php
@@ -1,6 +1,7 @@
 <?php
 require("config.php");
 
+echo "password from GET: " . $_GET['password'] . ", password: " . $password;
 if (strcmp($_GET['password'], $_password) == 0) {
 	$file = fopen("subscription.json", "w") or die("Unable to open file!");
 	$txt = $txt = '{ "subscription":"' . $_GET['subscription'] . '" }';
